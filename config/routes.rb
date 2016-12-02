11687057fff6f7f328d4dbc97c2e2f83095d8445
Daniel Alejandro Dominguez Diaz <danielfromarg@gmail.com>
2016-12-02 08:47:16 -0300

    Remove submodain

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  constraints subdomain: 'api' do
-    scope module: 'api' do
-      namespace :v1 do
-        get :dashboard, to: "dashboard#index"
-      end
+  scope module: 'api' do
+    namespace :v1 do
+      get :dashboard, to: "dashboard#index"
     end
   end
 end
